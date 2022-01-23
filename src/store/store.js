293979048e783c16865f9c176f4d93f92ecb63c7
Franco <francoadrianc@gmail.com>
2022-01-23 03:38:39 -0300

    Test persist redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,13 @@
 import { rootReducer } from "../reducers/rootReducer";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
+
+const storageConfig = {
+  key: "root", // debe tener
+  storage, // Mecanismo de caché
+};
 
 const composeEnhancers =
   (typeof window !== "undefined" &&
@@ -12,7 +18,16 @@ const reducers = combineReducers({
   rootReducer,
 });
 
-export const store = createStore(
-  reducers /* preloadedState, */,
+// export const store = createStore(
+//   reducers /* preloadedState, */,
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+
+const myPersistReducer = persistReducer(storageConfig, reducers);
+const store = createStore(
+  myPersistReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
+export default store;
